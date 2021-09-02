a004ee31868bcdf1b2d556e684b3acab32c671c3
milanboga <23mboga@athenian.org>
2021-09-01 18:41:46 -0700

    9/1 Homework first attempt

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,7 +5,24 @@ public class MyMain {
         Scanner scan = new Scanner(System.in);
 
         // Add some code here!
-
+        System.out.println("What's your name friend??????");
+        String name = scan.next();
+        System.out.println("Hello there " + name);
+        System.out.println("What is you favorite color, " + name + "?");
+        String color = scan.next();
+        System.out.println(color + " is a great color!");
+        System.out.println("What's your favorite sport?");
+        String sport = scan.next();
+        System.out.println("I love " + sport + " too!");
+        System.out.println("How old are you " + name + " ?");
+        int age = scan.nextInt();
+        System.out.println("No way, my mom is " + age + " too.");
+        System.out.println("Last question, what's your favorite food?");
+        String food = scan.next();
+        System.out.println("Oh, I'm actually allergic to " + food + " :(");
+        System.out.println("So, just to be clear, your name is " + name + ", your favorite color is " + color
+        + ", your favorite sport is " + sport + ", you are " + age + " years old, and your favorite food is " +
+        food);
         scan.close();
     }
 }
